ca5bd1bf22892e306b610691d363e412632472a8
doblux <dobluxGithub@protonmail.com>
2026-04-08 00:24:23 -0300

    http y dns sniffer

---
+++ dns_sniffer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import scapy.all as scapy
+
+def process_dns_packet(packet):
+    #print(packet.show()) # ver los paquetes
+    if packet.haslayer(scapy.DNSQR): # pyright: ignore
+        domain = packet[scapy.DNSQR].qname.decode() # pyright: ignore
+    
+        exclude_keywords = ["google", "cloud", "bing", "static"]
+        
+        if domain not in domains_seen and not any(keyword in domain for keyword in exclude_keywords):
+            domains_seen.add(domain)
+            print(f"[+] Dominio: {domain}")
+
+def sniff(interface):
+    scapy.sniff(iface=interface, filter="udp and port 53", prn=process_dns_packet, store=0)
+
+
+def main():
+    print(f"\n[+] Interceptando paquetes de la máquina víctima:\n")
+    sniff("enp3s0")
+
+if __name__ == "__main__":
+    global domains_seen
+    domains_seen = set()
+    
+    main()
